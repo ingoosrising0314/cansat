1c07f2befb6e36098a252e0d08da5f4b5a50d28b
DESKTOP-SATOS20\82108 <202004143@o.cnu.ac.kr>
2023-04-09 01:09:26 +0900

    camera와 작동 프로그램 작성 , object_detection 모듈화

---
+++ Raspberrypi/camera.py
@@ -0,0 +1,15 @@
+from picamera import PiCamera
+from time import sleep
+import cv2
+import object_detection as od
+
+
+camera=PiCamera()
+camera.resolution=(1920,1080)
+
+camera.start_preview()
+for i in range(5):
+	sleep(1)
+	camera.capture('/home/raspberrypi/Ingoos/image%s.jpg' %i)
+	od.object_detection('Sample_TFLite_model','image%s.jpg' %i)
+camera.stop_preview()
